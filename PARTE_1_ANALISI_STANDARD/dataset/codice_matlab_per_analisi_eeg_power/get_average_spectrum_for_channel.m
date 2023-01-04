076a9b49cbd7476e925ee597ea90d3ae9d5434ac
GiulioFederico <44112303+GiulioFede@users.noreply.github.com>
2023-01-04 13:25:56 +0100

    adding zero padding to Welch implementation

--- PARTE_1_ANALISI_STANDARD/dataset/codice_matlab_per_analisi_eeg_power/get_average_spectrum_for_channel.m
+++ PARTE_1_ANALISI_STANDARD/dataset/codice_matlab_per_analisi_eeg_power/get_average_spectrum_for_channel.m
@@ -16,27 +16,44 @@ function [average_spectrum_for_channel,hz] = get_average_spectrum_for_channel(tr
 
     number_of_channels = size(true_trials,1);
 
-    %creo una matrice 128x500, ossia contenente per ogni canale i 501
+    %lunghezza (in time points) singola trial
+    length_single_trial = size(true_trials(1,:,1),2);
+
+    %numero di trials
+    number_of_trials = size(true_trials(1,1,:),3);
+
+    %numero delle frequenze che andremo ad analizzare (NB: grazie allo zero
+    %padding ne avremo 1001 invece che 351)
+    hz = linspace(0, sampling_rate/2, floor(length_single_trial/2)+1); 
+    number_of_frequencies_of_interest = size(hz,2);
+
+    %creo una matrice 128x1001, ossia contenente per ogni canale i 1001
     %valori di potenza dello spettro medio ottenuto dalle sue 13 trial
-    average_spectrum_for_channel = zeros(number_of_channels, 501);
+    average_spectrum_for_channel = zeros(number_of_channels, number_of_frequencies_of_interest);
 
     %per ogni canale...
     for channel_i=1:number_of_channels
 
-        %calcolo i 13 spettri, ciascuno smussato con il metodo di Welch
-        
-        % number of FFT points (frequency resolution)
-        nfft = sampling_rate;
-        
-        window_length = 2000*0.35; % 35% della lunghezza del segnale
-        
-        overlapping = window_length/2; % 50% di overlapping
-        
-        %avendo 13 trial e 501 frequenze, power sarà una matrice 501x13
-        [power, hz] = pwelch(squeeze(true_trials(channel_i,:,:)), hamming(window_length), overlapping,nfft,sampling_rate, "power");
-        
-        %trasformo in potenza comprendibile graficamente
-        power=(power*2);
+        %{
+            avendo ogni canale "number_of_trials" numero di trials e volendo per ciascuna calcolare la potenza di "number_of_frequencies_of_interest"
+            frequenze di interesse, allora creo una matrice
+            number_of_frequencies_of_interest * number_of_trials dove per ogni
+            colonna C mi dice quali sono le potenze delle
+            "number_of_frequencies_of_interest" frequenze della trial C
+        %} 
+
+        power = zeros(number_of_frequencies_of_interest, number_of_trials);
+
+        %per ognuna delle trial ottengo lo spettro di Welch
+        for trial_i=1:number_of_trials
+
+            %ottengo lo spettro di potenza secondo il metodo Welch
+            welch_spectrum_trial_i = get_welch_spectrum(true_trials(channel_i,:,trial_i), sampling_rate);
+
+            %lo salvo
+            power(:,trial_i) = welch_spectrum_trial_i;
+        end
+ 
 
         %Se si vuole fare la baseline normalization (versione vecchia)
         if do_baseline_normalization == true
@@ -60,6 +77,8 @@ function [average_spectrum_for_channel,hz] = get_average_spectrum_for_channel(tr
             f3
             ...
         %}
+
+        %per ogni fi ho 16 valori, calcolo la media 
         average_power_spectrum_channel_i = mean(power,2);
 
         %lo salvo
